1384924ce3dbb02793e84fb7396fefe88394c367
JanPhillip20 <jan.phillip.me@outlook.de>
2021-11-18 10:02:36 +0100

    Initial commit

---
+++ CalculatorTestDrivenDevelopment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculatorTestDrivenDevelopment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
